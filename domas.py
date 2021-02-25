05e779334ca401fd2fdbfed57a7f63e8f43abdf4
EbicHecker <60292167+epic6969@users.noreply.github.com>
2021-02-25 01:23:01 +0100

    Update domas.py

--- domas.py
+++ domas.py
@@ -3,6 +3,8 @@ from discord.ext.commands import Bot
 from discord.ext import commands
 import asyncio
 from os import path
+import os
+from time import sleep
 import requests
 
 TOKEN = ""
@@ -28,4 +30,33 @@ def startSb():
 async def on_ready():
     print("Let's go bro!!")
 
+# Emoji - Sends any emoji as link/attachment.
+@sb.command()
+async def emoji(ctx):
+    await print("hello bro")
+
+# Search - Search for emoji in your files.
+@sb.command()
+async def search(ctx):
+    await print("hello bro")
+
+# Dump - Dumps all emojis in a guild.
+@sb.command()
+async def dump(ctx):
+    await print("hello bro")
+
+# Clear - Clears console screen.
+@sb.command()
+async def clear(ctx):
+    if os.name == "posix":
+        _ = os.system("clear")
+    else:
+        _ = os.system("cls")
+
+# Help - SHows a list of all commands.
+@sb.command()
+async def help(ctx):
+    await print("Here's a list of all the commands:\n*Help - Shows a list of all commands.\n*Dump - Dumps all emojis in a guild\n*Search - Search for an emoji.\n*Emoji - Use an emoji from your files.\n*Clear - Clears console screen.")
+
+clear()
 startSb()
